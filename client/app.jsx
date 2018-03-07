594b6abdb80ba096e9000d7420bccce70fa12b7c
ulises-jeremias <ulisescf.24@gmail.com>
2018-03-07 15:13:22 -0300

    Updated files - atom beautify passed

--- client/app.jsx
+++ client/app.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import {AppContainer} from 'react-hot-loader';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 
 import store from '../shared/store';
 import App from '../shared/routes';
 
 const render = (Component) => {
-  ReactDOM.hydrate(
-    <AppContainer>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Component />
-        </BrowserRouter>
-      </Provider>
-    </AppContainer>,
-    document.getElementById('app'),
-  );
+  ReactDOM.hydrate(<AppContainer>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Component/>
+      </BrowserRouter>
+    </Provider>
+  </AppContainer>, document.getElementById('app'),);
 };
 
 document.addEventListener('DOMContentLoaded', () => {
