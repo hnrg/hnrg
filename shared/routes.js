d586b7afed19a58111401cdd14994beeae032432
ulises-jeremias <ulisescf.24@gmail.com>
2018-03-07 15:25:26 -0300

    Added shared basic files

---
+++ shared/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  Switch,
+  Route
+} from 'react-router-dom';
+
+const Routes = () => (
+  <Switch>
+    <Route exact path = "/" component = {} />
+  </Switch>
+);
+
+export default Routes;
