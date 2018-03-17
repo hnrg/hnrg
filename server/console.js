32db5d523eb2b82269c7466bac95245efd047d51
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-03-17 18:01:49 -0300

    Added a console with a db connection

---
+++ server/console.js
@@ -0,0 +1,15 @@
+var repl = require('repl');
+
+const serverConfig = require('./config');
+const mongoose = require('mongoose');
+
+mongoose.connect(serverConfig.mongoURL, function(err){
+  if (err){ throw err; }
+
+  var replServer = repl.start({
+    prompt: "HDRG > ",
+  });
+
+  replServer.context.epa = serverConfig.mongoURL;
+  replServer.context.db = mongoose;
+});
