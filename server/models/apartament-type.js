17baa044b1ad284bb6f61d582e5fdbf46e1d6c39
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-03-17 13:48:46 -0300

    Beautify code

--- server/models/apartament-type.js
+++ server/models/apartament-type.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const apartamentTypeSchema = new Schema({
-  name: { type: String, required: 'El campo `nombre` es requerido' }
+  name: { type: String, required: 'El campo `nombre` es requerido' },
 });
 
 module.exports = mongoose.model('ApartamentType', apartamentTypeSchema);
