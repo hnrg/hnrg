433565e2feea1bf397e6d9d5e301349084dbd744
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-03-17 12:59:01 -0300

    Added Healt Control model

---
+++ server/models/health-control.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const healthControlSchema = new Schema({
+  date: {
+    type: Date,
+    required: 'El campo `fecha` es requerido'
+  },
+  weight: {
+    type: Schema.Types.Decimal128,
+    required: 'El campo `peso` es requerido'
+  },
+  pc: {
+    type: Schema.Types.Decimal128,
+    required: 'El campo `pc` es requerido'
+  },
+  ppc: {
+    type: Schema.Types.Decimal128,
+    required: 'El campo `ppc` es requerido'
+  },
+  height: {
+    type: Schema.Types.Decimal128,
+    require: 'El campo `talla` es requerido'
+  },
+  completeVaccines: {
+    type: Boolean,
+    default: false
+  },
+  vaccinesObservations: { type: String },
+  accordingMaturationContext: {
+    type: Boolean,
+    default: false
+  },
+  maturationObservations: { type: String },
+  commonPhysicalExamination: {
+    type: Boolean,
+    default: false
+  },
+  physicalExaminationObservations: { type: String },
+  feeding: { type: String },
+  generalObservations: { type: String },
+  patientId: {
+    type: Schema.Types.ObjectId,
+    required: 'El campo `paciente` es requerido'
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: 'El campo `user` es requerido'
+  },
+  active: {
+    type: Boolean,
+    default: true
+  }
+});
+
+module.exports = mongoose.model('HealthControl', healthControlSchema);
