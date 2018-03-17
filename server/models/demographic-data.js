45e758879413e0f071f49084e7ad267cbad6cf16
ulises-jeremias <ulisescf.24@gmail.com>
2018-03-17 17:06:51 -0300

    Updated

--- server/models/demographic-data.js
+++ server/models/demographic-data.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ApartmentType = require('./apartament-type');
+const HeatingType= require('./heating-type');
+const WaterType = require('./water-type');
+
 const demographicDataSchema = new Schema({
   refrigerator: {
     type: Boolean,
@@ -16,14 +20,17 @@ const demographicDataSchema = new Schema({
   },
   apartmentTypeId: {
     type: Schema.Types.ObjectId,
+    ref: ApartmentType,
     required: 'El campo `tipo de vivienda` es requerido'
   },
   heatingTypeId: {
     type: Schema.Types.ObjectId,
+    ref: HeatingType,
     required: 'El campo `tipo de calefacción` es requerido'
   },
   waterTypeId: {
     type: Schema.Types.ObjectId,
+    ref: WaterType,
     required: 'El campo `tipo de agua` es requerido'
   },
 });
