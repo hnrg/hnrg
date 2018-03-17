bd799c81219c2a1ec666baa3b8742ac98f0e78f5
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-03-17 13:11:59 -0300

    Added Turns model

---
+++ server/models/turn.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const turnSchema = new Schema({
+  date: {
+    type: Date,
+    required: 'El campo `fecha` es requerido'
+  },
+  documentNumber: {
+    type: Number,
+    required: 'El campo `número de documento` es requerido'
+  },
+  chatId: { type: Number }
+});
+
+module.exports = mongoose.model('Turn', turnSchema);
