64cce23907f818ae015951c68165deeeac0555e2
ulises-jeremias <ulisescf.24@gmail.com>
2018-03-17 13:52:04 -0300

    Updated user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
   email: {
     type: String,
     trim: true,
@@ -36,6 +37,7 @@ const userSchema = new mongoose.Schema({
   active: { type: Boolean, default: false },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
+  rolesId: { type: [Schema.Types.ObjectId] },
 });
 
 module.exports = mongoose.model('User', userSchema);
