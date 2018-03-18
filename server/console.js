17bd228502f0505fc0d9a6d666daef2285b6e996
ulises-jeremias <ulisescf.24@gmail.com>
2018-03-18 00:30:12 -0300

    Updated config

--- server/console.js
+++ server/console.js
@@ -3,12 +3,12 @@ var repl = require('repl');
 const serverConfig = require('./config');
 const mongoose = require('mongoose');
 
-mongoose.connect(serverConfig.mongoURL, function(err){
-  if (err){ throw err; }
+mongoose.connect(serverConfig.mongoURL, function(err) {
+  if (err) {
+    throw err;
+  }
 
-  var replServer = repl.start({
-    prompt: "HDRG > ",
-  });
+  var replServer = repl.start({prompt: "HDRG > "});
 
   replServer.context.epa = serverConfig.mongoURL;
   replServer.context.db = mongoose;
