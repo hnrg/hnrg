0b0ec7aee63f079b51110a201ebd2c9cae775766
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-03-23 12:01:02 -0300

    Added basic file from telegram routes

---
+++ server/routes/telegram.routes.js
@@ -0,0 +1,9 @@
+const {Router} = require('express');
+//const TelegramController = require('../controllers/telegram.controller');
+
+const router = Router();
+
+// Get all Appointment
+router.route('/telegram').get(()=>{});
+
+module.exports = router;
