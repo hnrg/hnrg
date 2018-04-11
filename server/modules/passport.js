7fcf5b9a02cc581751d0589e95d9f671a6226d21
ulises-jeremias <ulisescf.24@gmail.com>
2018-04-11 10:26:41 -0300

    Added module

---
+++ server/modules/passport.js
@@ -0,0 +1,60 @@
+const passport = require('passport'),
+      User = require('../models/user'),
+      secret = require('../secret'),
+      JwtStrategy = require('passport-jwt').Strategy,
+      ExtractJwt = require('passport-jwt').ExtractJwt,
+      LocalStrategy = require('passport-local');
+
+
+const localOptions = {
+  usernameField: 'email' // Setting username field to email rather than username
+}
+
+
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  User.findOne({ email: email }, function(err, user) {
+    if(err) {
+      return done(err);
+    }
+
+    if(!user) {
+      return done(null, false, { error: 'No user exists with that email address' });
+    }
+
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) {
+        return done(err);
+      }
+
+      if (!isMatch) {
+        return done(null, false, { error: "Invalid password" });
+      }
+
+      return done(null, user);
+    });
+  });
+});
+
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt"),   // Telling Passport to check authorization headers for JWT
+  secretOrKey: secret.secret   // Telling Passport where to find the secret
+};
+
+
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  User.findById(payload._doc._id, function(err, user) {
+    if (err) {
+      return done(err, false);
+    }
+
+    if (!user) {
+      return done(null, false);
+    }
+
+    return done(null, user);
+  });
+});
+
+passport.use(jwtLogin);
+passport.use(localLogin);
