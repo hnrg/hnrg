ec170c5f5806a1eb21a3211584c2fe24df449db3
ulises-jeremias <ulisescf.24@gmail.com>
2018-04-11 10:26:32 -0300

    Added auth basic modules

---
+++ server/routes/auth.routes.js
@@ -0,0 +1,15 @@
+const {Router} = require('express');
+const passport = require('passport');
+
+const passportService = require('../modules/passport');
+const AuthController = require('../controllers/auth.controller');
+
+// Middleware to require login/auth
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireLogin = passport.authenticate('local', { session: false });
+
+const router = Router();
+
+router.route('/login').post(requireLogin, AuthController.login);
+
+module.exports = router;
