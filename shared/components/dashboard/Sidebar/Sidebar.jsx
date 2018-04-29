d6e3dd9657f36423fac6ce433e1095859c092467
ulises-jeremias <ulisescf.24@gmail.com>
2018-04-29 18:01:17 -0300

    Updated sidebar

--- shared/components/dashboard/Sidebar/Sidebar.jsx
+++ shared/components/dashboard/Sidebar/Sidebar.jsx
@@ -28,33 +28,31 @@ class SidebarLeftScaleDown extends Component {
 
   render() {
     const {visible} = this.state;
-    /*return (<div>
+    return (<div>
+      <Sidebar
+        as={Menu}
+        animation='scale down'
+        width='thin'
+        visible={visible}
+        icon='labeled'
+        vertical
+        inverted
+      >
+        <Menu.Item name='home'>
+          <Icon name='home'/>
+          Home
+        </Menu.Item>
+        <Menu.Item name='gamepad'>
+          <Icon name='gamepad'/>
+          Games
+        </Menu.Item>
+        <Menu.Item name='camera'>
+          <Icon name='camera'/>
+          Channels
+        </Menu.Item>
+      </Sidebar>
       <Button onClick={this.toggleVisibility}>Toggle Visibility</Button>
-      <Sidebar.Pushable as={Segment}>
-        <Sidebar as={Menu} animation='scale down' width='thin' visible={visible} icon='labeled' vertical="vertical" inverted="inverted">
-          <Menu.Item name='home'>
-            <Icon name='home'/>
-            Home
-          </Menu.Item>
-          <Menu.Item name='gamepad'>
-            <Icon name='gamepad'/>
-            Games
-          </Menu.Item>
-          <Menu.Item name='camera'>
-            <Icon name='camera'/>
-            Channels
-          </Menu.Item>
-        </Sidebar>
-        <Sidebar.Pusher>
-          <Segment basic="basic">
-            <Header as='h3'>Application Content</Header>
-            <Image src='/assets/images/wireframe/paragraph.png'/>
-          </Segment>
-        </Sidebar.Pusher>
-      </Sidebar.Pushable>
-    </div>);*/
-
-    return (<div></div>);
+    </div>);
   }
 }
 
