af601db86579cfaf79c5d7df6ce834192cced11c
ulises-jeremias <ulisescf.24@gmail.com>
2018-04-30 16:50:42 -0300

    Updated server config

---
+++ server/config/app.js
@@ -0,0 +1,9 @@
+module.exports = {
+  webpage: {
+    name: "Hospital de Ni\u00f1os Ricardo Gutierrez",
+    description: "El Hospital Gutierrez de La Plata fue inaugurado el 6 de Agosto de 1954. En ese entonces, el hospital funcionaba solo en el \u00e1rea central del edificio, siendo que las dos alas anexas eran ocupadas por casas de admisi\u00f3n de menores.",
+    email: "contact@hnrg.com",
+    amountPerPage: 7,
+  },
+  maintenance: false,
+};
