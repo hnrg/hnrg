00163f6f8f41e4f74850111e211036ee051592b4
ulises-jeremias <ulisescf.24@gmail.com>
2018-05-05 00:15:31 -0300

    Added roles tests

--- tests/server/roles.test.js
+++ tests/server/roles.test.js
@@ -5,13 +5,13 @@ const superagent = require('superagent');
 
 const agent = superagent.agent();
 
-const {admin} = require('../../server/config/secret');
+const { admin } = require('../../server/config/secret');
 const server = require('../../server');
 const Seed = require('../../server/dummyData');
 
 const expect = chai.expect;
 
-var token;
+let token;
 
 function login(request, done) {
   request(server)
@@ -26,23 +26,18 @@ function login(request, done) {
       token = res.body.token;
       done();
     });
-};
+}
 
 describe('Roles APIs', () => {
   let agent;
 
-  beforeEach((done) => {
-    login(request, (loginAgent) => {
-      agent = loginAgent;
-      done();
-    });
-  });
+  beforeEach(done => login(request, done));
 
   test('Should load all available roles', (done) => {
     request(server)
       .get('/api/roles')
       .set('Authorization', token)
-      .end(function(err, res) {
+      .end((err, res) => {
         expect(res.body.roles).to.be.an('array');
         expect(res.statusCode).to.be.equal(200);
         done();
