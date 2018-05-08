129ed235ccc974080724381495a0956d178bbc4e
ulises-jeremias <ulisescf.24@gmail.com>
2018-05-08 18:08:40 -0300

    Updated tests

--- tests/server/auth.test.js
+++ tests/server/auth.test.js
@@ -2,9 +2,22 @@ const request = require('supertest');
 const { expect } = require('chai');
 
 const server = require('../../server');
+const Seed = require('../../server/dummyData');
+const Rol = require('../../server/models/rol');
+const User = require('../../server/models/user');
+
+const cleanAndSeedDb = async function cleanAndSeedDb() {
+  await User.remove({});
+  await Rol.remove({});
+  await Seed();
+};
 
 describe('Login Tests', () => {
   describe('Successes', () => {
+    beforeEach(async () => {
+      await cleanAndSeedDb();
+    });
+
     test('should return the token if valid', (done) => {
       request(server)
         .post('/api/auth/login')
