23ef95621ebcc299cdd33ded424f4547db085065
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-05-11 20:35:31 -0300

    Change mongose to redis

--- server/modules/telegram/callbacks/desvincular.js
+++ server/modules/telegram/callbacks/desvincular.js
@@ -1,13 +1,11 @@
-module.exports = function desvincular(bot, msg, user) {
+module.exports = function desvincular(bot, msg, client) {
   const chatId = msg.from.id;
   const message = {
     chat_id: chatId,
     message_id: msg.message.message_id,
   };
 
-  user.then((userData) => {
-    bot.editMessageText(`Hasta luego ${userData.patient.firstName} ${userData.patient.lastName}`, message);
-    userData.remove();
-  });
+  client.del(chatId);
+  bot.editMessageText('Hasta luego', message);
 };
 
--- server/modules/telegram/callbacks/info.js
+++ server/modules/telegram/callbacks/info.js
@@ -1,18 +1,21 @@
 const moment = require('moment-timezone');
+const Patient = require('../../../models/patient');
 
-module.exports = function info(bot, msg, user) {
+module.exports = function info(bot, msg, client) {
   const chatId = msg.from.id;
   const message = {
     chat_id: chatId,
     message_id: msg.message.message_id,
   };
 
-  user.then((userData) => {
-    let userInfo = `Nombre y apellido: ${userData.patient.firstName} ${userData.patient.lastName}\n`;
-    userInfo += `${userData.patient.documentType.name}: ${userData.patient.documentNumber}\n`;
-    userInfo += `Fecha de nacimiento: ${moment(userData.patient.birthday).format('DD/MM/YYYY')}\n`;
-    userInfo += '\n\nSi quiere cambiar algun dato, debe comunicarse con la secretaría del Hospital.';
-    bot.editMessageText(userInfo, message);
+  client.get(chatId, (err, data) => {
+    Patient.findOne({ documentNumber: data }).populate('documentType').exec((err, patient) => {
+      let userInfo = `Nombre y apellido: ${patient.firstName} ${patient.lastName}\n`;
+      userInfo += `${patient.documentType.name}: ${patient.documentNumber}\n`;
+      userInfo += `Fecha de nacimiento: ${moment(patient.birthday).format('DD/MM/YYYY')}\n`;
+      userInfo += '\n\nSi quiere cambiar algun dato, debe comunicarse con la secretaría del Hospital.';
+      bot.editMessageText(userInfo, message);
+    });
   });
 };
 
