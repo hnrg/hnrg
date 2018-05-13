c9e45d031cd5ea9af3829488698fd81afc898db8
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-05-12 23:53:47 -0300

    Added roles constants

--- shared/constants/index.js
+++ shared/constants/index.js
@@ -4,3 +4,4 @@ export * from './global.constants';
 export * from './profile.constants';
 export * from './patients.constants';
 export * from './health-controls.constants';
+export * from './roles.constants';
---
+++ shared/constants/roles.constants.js
@@ -0,0 +1,7 @@
+export const GET_ROLES_REQUEST = 'get-roles-request';
+export const GET_ROLES_SUCCESS = 'get-roles-success';
+export const GET_ROLES_FAILURE = 'get-roles-failure';
+
+export const GET_ROL_REQUEST = 'get-rol-request';
+export const GET_ROL_SUCCESS = 'get-rol-success';
+export const GET_ROL_FAILURE = 'get-rol-failure';
