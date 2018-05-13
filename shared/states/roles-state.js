348052f0fc6d6fd3917fbe11f4ae1785dcf54fd3
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-05-12 23:55:22 -0300

    Added roles states

---
+++ shared/states/roles-state.js
@@ -0,0 +1,21 @@
+export default {
+  originalRol: {
+    id: null,
+    name: null,
+    permissions: null,
+  },
+  roles: [],
+  disabled: false,
+  error: null,
+  isValid: false,
+  isFetching: false,
+  fields: {
+    name: null,
+    nameHasError: false,
+    nameErrorMsg: '',
+    permissions: null,
+    permissionsHasError: false,
+    permissionsNameErrorMsg: '',
+  },
+};
+
