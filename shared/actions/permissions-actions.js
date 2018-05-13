fe73868334a3fbb20cf22c28cc99157c211d30dc
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-05-13 00:49:32 -0300

    Added patients actions

---
+++ shared/actions/permissions-actions.js
@@ -0,0 +1,53 @@
+import {
+  GET_PERMISSIONS_REQUEST,
+  GET_PERMISSIONS_SUCCESS,
+  GET_PERMISSIONS_FAILURE,
+} from '../constants';
+
+import { permissionsRequest } from '../lib/request/permissions-request';
+import { authToken } from '../lib/store/auth-token';
+
+/**
+ * ## retreiving profile actions
+ */
+export function getPermissionsRequest() {
+  return {
+    type: GET_PERMISSIONS_REQUEST,
+  };
+}
+
+export function getPermissionsSuccess(permissions) {
+  return {
+    type: GET_PERMISSIONS_SUCCESS,
+    payload: permissions,
+  };
+}
+
+export function getPermissionsFailure(error) {
+  return {
+    type: GET_PERMISSIONS_FAILURE,
+    payload: error,
+  };
+}
+
+
+/**
+ * ## State actions
+ * controls which form is displayed to the user
+ * as in login, register, logout or reset password
+ */
+export function getPermissions(sessionToken) {
+  return (dispatch) => {
+    dispatch(getPermissionsRequest());
+    // store or get a sessionToken
+    return authToken.getSessionToken(sessionToken)
+      .then(token => permissionsRequest.init(token).getPermissions())
+      .then((json) => {
+        dispatch(getPermissionsSuccess(json.permissions));
+      })
+      .catch((error) => {
+        dispatch(getPermissionsFailure(error));
+      });
+  };
+}
+
---
+++ shared/states/permissions-state.js
@@ -0,0 +1,8 @@
+export default {
+  permissions: [],
+  disabled: false,
+  error: null,
+  isValid: false,
+  isFetching: false,
+};
+
