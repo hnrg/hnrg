3b14fb4606d806a1d9a5cec65454d15a30dbdaed
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-05-13 00:49:05 -0300

    Added permissions request

---
+++ shared/lib/request/permissions-request.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import _ from 'lodash';
+
+export default class PermissionsRequest {
+  init(token = null) {
+    this._sessionToken = _.isNull(token)
+      ? null
+      : token;
+
+    return this;
+  }
+
+  getPermissions() {
+    return axios.get('/api/permissions', {
+      headers: {
+        Authorization: this._sessionToken,
+      },
+    }).then((response) => {
+      if (response.status === 200 || response.status === 201) {
+        return response.data;
+      }
+      throw (response.data.error);
+    }).catch((error) => {
+      throw error;
+    });
+  }
+}
+
+// The singleton variable
+export const permissionsRequest = new PermissionsRequest();
+
