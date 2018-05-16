cede4730ba19beb8a99605b4a7572372b49f242c
ulises-jeremias <ulisescf.24@gmail.com>
2018-05-16 16:33:03 -0300

    Updated APIs

--- server/models/rol.js
+++ server/models/rol.js
@@ -6,6 +6,7 @@ const Permission = require('./permission');
 
 const rolSchema = new Schema({
   name: { type: String, required: 'El campo `nombre` es requerido', unique: true },
+  deleted: { type: Boolean, default: false },
   permissions: [{ type: Schema.Types.ObjectId, ref: 'Permission' }],
 });
 
