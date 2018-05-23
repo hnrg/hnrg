c0527763a574c46ce7de7f2be95503efb2c4812a
ulises-jeremias <ulisescf.24@gmail.com>
2018-05-23 09:39:16 -0300

    Updated show component

--- shared/components/User/Show.jsx
+++ shared/components/User/Show.jsx
@@ -1,5 +1,16 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment-timezone';
+import {
+  Button,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+} from 'semantic-ui-react';
+
+import icon from 'static/icons/icon.png';
 
 class Show extends Component {
   constructor(props) {
@@ -29,8 +40,39 @@ class Show extends Component {
   }
 
   render() {
+    const { user } = this.state;
+
     return(
-      <div>{this.state.user.email}</div>
+      <center>
+        <Header as='h2'>
+          {user.firstName} {user.lastName}
+        </Header>
+        <Button circular color='blue' icon='bar chart' />
+        <Header as='h4' content={user.username} />
+        <Divider hidden />
+        <Grid>
+          <Grid.Row>
+            <Grid.Column style={{padding: '0 0 15px 0'}} computer={5} largeScreen={5} tablet={16} mobile={16}>
+              <Header as='h5' content='Nombre de Usuario'/>
+              {user.username}
+            </Grid.Column>
+            <Grid.Column style={{padding: '0 0 15px 0'}} computer={6} largeScreen={6} tablet={16} mobile={16}>
+              <Header as='h5' content='Email'/>
+              {user.email}
+            </Grid.Column>
+            <Grid.Column style={{padding: '0 0 15px 0'}} computer={5} largeScreen={5} tablet={16} mobile={16}>
+              <Header as='h5' content='Fecha de Ingreso'/>
+              {moment(user.createdAt).format('LLLL')}
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Divider hidden />
+        <Button color='blue'>
+          <Icon name='certificate' /> Roles
+        </Button>
+        <Divider hidden />
+        <Image size='mini' src={icon} />
+      </center>
     );
   }
 }
