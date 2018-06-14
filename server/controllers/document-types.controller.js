fee9d546b8d36d15dcf1500e273de9b98ab7ad07
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-06-14 20:22:15 -0300

    Added document-types controller

---
+++ server/controllers/document-types.controller.js
@@ -0,0 +1,22 @@
+const DocumentType = require('../models/document-type');
+
+/**
+ * Get all documents type
+ * @param req
+ * @param res
+ * @returns void
+ */
+exports.getDocumentTypes = async function getDocumentTypes(req, res) {
+  try {
+    const documentTypes = await DocumentType.find({})
+      .exec();
+
+    res.status(200).send({ documentTypes });
+  } catch (e) {
+    if (e.name === 'NotAllowedError') {
+      return res.status(403).send(e);
+    }
+
+    res.status(500).send(e);
+  }
+};
