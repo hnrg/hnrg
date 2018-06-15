5349b3c54dcf3fc99e097fa9d82cd0851ad7baf4
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-06-15 00:23:16 -0300

    Added heating type controller

---
+++ server/controllers/heating-type.controller.js
@@ -0,0 +1,22 @@
+const HeatingType = require('../models/heating-type');
+
+/**
+ * Get all heating types
+ * @param req
+ * @param res
+ * @returns void
+ */
+exports.getHeatingTypes = async function getHeatingTypes(req, res) {
+  try {
+    const heatingTypes = await HeatingType.find({})
+      .exec();
+
+    res.status(200).send({ heatingTypes });
+  } catch (e) {
+    if (e.name === 'NotAllowedError') {
+      return res.status(403).send(e);
+    }
+
+    res.status(500).send(e);
+  }
+};
