a7abba03a8181eeb180b4b0364779cd53a9ad612
ulises-jeremias <ulisescf.24@gmail.com>
2018-06-15 20:37:46 -0300

    Updated configs

--- server/modules/middleware/configuration.js
+++ server/modules/middleware/configuration.js
@@ -3,6 +3,7 @@ const Configuration = require('../../models/configuration');
 module.exports = async function configurationMiddleware(req, res, next) {
   try {
     await Configuration.findOne({})
+      .sort('-updatedAt')
       .exec((err, configuration) => {
         req.configuration = configuration;
         req.pageNumber = req.query.pageNumber || 0;
