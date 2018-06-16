86f48afcde5b00552fb91a2f12129d94677559ae
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-06-16 13:58:00 -0300

    Added new constats!

--- shared/reducers/constants/patients.constants.js
+++ shared/reducers/constants/patients.constants.js
@@ -5,3 +5,15 @@ export const GET_PATIENTS_FAILURE = 'get-patients-failure';
 export const GET_PATIENT_REQUEST = 'get-patient-request';
 export const GET_PATIENT_SUCCESS = 'get-patient-success';
 export const GET_PATIENT_FAILURE = 'get-patient-failure';
+
+export const PATIENT_ADD_REQUEST = 'patient-add-request';
+export const PATIENT_ADD_SUCCESS = 'patient-add-success';
+export const PATIENT_ADD_FAILURE = 'patient-add-failure';
+
+export const PATIENT_UPDATE_REQUEST = 'patient-update-request';
+export const PATIENT_UPDATE_SUCCESS = 'patient-update-success';
+export const PATIENT_UPDATE_FAILURE = 'patient-update-failure';
+
+export const ON_PATIENT_FORM_CLEAR = 'on-patient-form-clear';
+export const ON_PATIENT_FORM_FIELD_CHANGE = 'on-patient-form-field-change';
+
