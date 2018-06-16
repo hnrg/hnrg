710b65985f1af9543a0f653c876dbe538fbb7edd
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-06-16 13:55:38 -0300

    Added new constants

--- shared/reducers/reducers/patients.js
+++ shared/reducers/reducers/patients.js
@@ -1,4 +1,5 @@
 import fieldValidation from 'reducers/lib/field-validation/patients';
+import formValidation from './patients-form';
 
 import {
   GET_PATIENTS_REQUEST,
@@ -8,6 +9,17 @@ import {
   GET_PATIENT_REQUEST,
   GET_PATIENT_SUCCESS,
   GET_PATIENT_FAILURE,
+
+  PATIENT_ADD_REQUEST,
+  PATIENT_ADD_SUCCESS,
+  PATIENT_ADD_FAILURE,
+
+  PATIENT_UPDATE_REQUEST,
+  PATIENT_UPDATE_SUCCESS,
+  PATIENT_UPDATE_FAILURE,
+
+  ON_PATIENT_FORM_CLEAR,
+  ON_PATIENT_FORM_FIELD_CHANGE,
 
   LOGOUT_SUCCESS,
 
@@ -33,7 +45,11 @@ export default function patientsReducer(state = InitialState, action) {
      * ### Request starts
      * set the form to fetching and clear any errors
      */
-    case GET_PATIENTS_REQUEST: {
+    case GET_PATIENTS_REQUEST:
+    case GET_PATIENT_REQUEST:
+    case PATIENT_ADD_REQUEST:
+    case PATIENT_UPDATE_REQUEST:
+    {
       return { ...state, isFetching: true, error: null };
     }
 
@@ -56,6 +72,98 @@ export default function patientsReducer(state = InitialState, action) {
       };
     }
 
+    case PATIENT_ADD_SUCCESS:
+    case PATIENT_UPDATE_SUCCESS:
+    {
+      return { ...state, isFetching: false };
+    }
+
+    case GET_PATIENT_SUCCESS: {
+      nextPatientState = {
+        ...state,
+        fields: {
+          ...state.fields,
+          firstName: action.payload.firstName,
+          lastName: action.payload.lastName,
+          address: action.payload.address,
+          phone: action.payload.phone,
+          birthday: action.payload.birthday,
+          sex: action.payload.sex,
+          medicalInsurance: action.payload.medicalInsurance,
+          documentType: action.payload.documentType,
+          documentNumber: action.payload.documentNumber,
+        },
+        originalPatient: {
+          ...state.originalPatient,
+          id: action.payload._id,
+          firstName: action.payload.firstName,
+          lastName: action.payload.lastName,
+          address: action.payload.address,
+          phone: action.payload.phone,
+          birthday: action.payload.birthday,
+          sex: action.payload.sex,
+          demographicData: action.payload.demographicData,
+          medicalInsurance: action.payload.medicalInsurance,
+          documentType: action.payload.documentType,
+          documentNumber: action.payload.documentNumber,
+          deleted: action.payload.deleted,
+        },
+        isFetching: false,
+        error: null,
+      };
+
+      return fieldValidation(nextPatientState, action);
+    }
+
+    case ON_PATIENT_FORM_CLEAR: {
+      return {
+        ...state,
+        fields: {
+          firstName: '',
+          firstNameHasError: false,
+          firstNameErrorMsg: '',
+          lastName: '',
+          lastNameHasError: false,
+          lastNameErrorMsg: '',
+          address: '',
+          addressHasError: false,
+          addressErrorMsg: '',
+          phone: '',
+          phoneHasError: false,
+          phoneErrorMsg: '',
+          birthday: '',
+          birthdayHasError: false,
+          birthdayErrorMsg: '',
+          sex: '',
+          sexHasError: false,
+          sexErrorMsg: '',
+          medicalInsurance: null,
+          medicalInsuranceHasError: false,
+          medicalInsuranceErrorMsg: '',
+          documentType: null,
+          documentTypeHasError: false,
+          documentTypeErrorMsg: '',
+          documentNumber: null,
+          documentNumberHasError: false,
+          documentNumberErrorMsg: '',
+        },
+      };
+    }
+
+    case ON_PATIENT_FORM_FIELD_CHANGE: {
+      const { field, value } = action.payload;
+
+      nextPatientState = {
+        ...state,
+        fields: {
+          ...state.fields,
+          [field]: value,
+        },
+      };
+
+      return formValidation(fieldValidation(nextPatientState, action), action);
+    }
+
     /**
      * User logged out, so reset form fields and original patient.
      *
@@ -72,58 +180,11 @@ export default function patientsReducer(state = InitialState, action) {
      * ### Request fails
      * we're done fetching and the error needs to be displayed to the user
      */
-    case GET_PATIENTS_FAILURE: {
-      return {
-        ...state,
-        isFetching: false,
-        error: action.payload,
-      };
-    }
-
-    /**
-     * ### Request starts
-     */
-    case GET_PATIENT_REQUEST: {
-      return { ...state, isFetching: true, error: null };
-    }
-
-    case GET_PATIENT_SUCCESS: {
-      nextPatientState = {
-        ...state,
-        fields: {
-          ...state.fields,
-          firstName: action.payload.firstName,
-          lastName: action.payload.lastName,
-          address: action.payload.address,
-          phone: action.payload.phone,
-          birthday: action.payload.birthday,
-          sex: action.payload.sex,
-          documentType: action.payload.documentType,
-          documentNumber: action.payload.documentNumber,
-        },
-        originalPatient: {
-          ...state.originalPatient,
-          firstName: action.payload.firstName,
-          lastName: action.payload.lastName,
-          address: action.payload.address,
-          phone: action.payload.phone,
-          birthday: action.payload.birthday,
-          sex: action.payload.sex,
-          state: action.payload.state,
-          demographicData: action.payload.demographicData,
-          medicalInsurance: action.payload.medicalInsurance,
-          documentType: action.payload.documentType,
-          documentNumber: action.payload.documentNumber,
-          deleted: action.payload.deleted,
-        },
-        isFetching: false,
-        error: null,
-      };
-
-      return fieldValidation(nextPatientState, action);
-    }
-
-    case GET_PATIENT_FAILURE: {
+    case GET_PATIENTS_FAILURE:
+    case GET_PATIENT_FAILURE:
+    case PATIENT_ADD_FAILURE:
+    case PATIENT_UPDATE_FAILURE:
+    {
       return {
         ...state,
         isFetching: false,
