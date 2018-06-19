9811eb12b7a0a76438cdea183cc1893a3f39f34f
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-06-19 20:12:38 -0300

    Added demographicData option + change icon colors

--- shared/components/Dashboard/Sidebar/Sidebar.jsx
+++ shared/components/Dashboard/Sidebar/Sidebar.jsx
@@ -28,35 +28,42 @@ class Sidebar extends Component {
     return (
       <Menu fixed='left' borderless className={'dashboard side' + (this.props.smallMenu ? ' small-side ' : '')} vertical>
         <Menu.Item as={Link} to={'/dashboard'} name='dashboard' active={activeItem === 'dashboard'}>
-          <TextIcon hideText={this.props.smallMenu} color='teal' name='home'>
+          <TextIcon hideText={this.props.smallMenu} color={ activeItem == 'dashboard' ? 'teal' : undefined } name='home'>
               Dashboard
           </TextIcon>
         </Menu.Item>
 
         <Menu.Item as={Link} to={'/dashboard/settings'} name='settings'
                    active={activeItem === 'settings'}>
-          <TextIcon hideText={this.props.smallMenu} name='setting'>
+          <TextIcon hideText={this.props.smallMenu} color={ activeItem == 'settings' ? 'grey' : undefined } name='setting'>
             Configuración
           </TextIcon>
         </Menu.Item>
 
         <Menu.Item as={Link} to={'/dashboard/users'} name='users'
                    active={activeItem === 'users'}>
-          <TextIcon hideText={this.props.smallMenu} name='users'>
+          <TextIcon hideText={this.props.smallMenu} color={ activeItem == 'users' ? 'blue' : undefined } name='users'>
             Usuarios
           </TextIcon>
         </Menu.Item>
 
         <Menu.Item as={Link} to={'/dashboard/patients'} name='patients'
                    active={activeItem === 'patients'}>
-          <TextIcon hideText={this.props.smallMenu} name='heartbeat'>
+          <TextIcon hideText={this.props.smallMenu} color={ activeItem == 'patients' ? 'red'  : undefined } name='heartbeat'>
             Pacientes
+          </TextIcon>
+        </Menu.Item>
+
+        <Menu.Item as={Link} to={'/dashboard/demographic-data'} name='demographic-data'
+                   active={activeItem === 'demographic-data'}>
+          <TextIcon hideText={this.props.smallMenu} color={activeItem == 'demographic-data' ? 'orange' : undefined } name='map marker alternate'>
+            Datos demográficos
           </TextIcon>
         </Menu.Item>
 
         <Menu.Item as={Link} to={'/dashboard/roles'} name='roles'
                    active={activeItem === 'roles'}>
-          <TextIcon hideText={this.props.smallMenu} name='certificate'>
+          <TextIcon hideText={this.props.smallMenu} color={activeItem == 'roles' ? 'yellow' : undefined } name='certificate'>
             Roles
           </TextIcon>
         </Menu.Item>
