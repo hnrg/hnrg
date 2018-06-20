f8e294010aeae04aa24d3d294140c5a88fc97078
ulises-jeremias <ulisescf.24@gmail.com>
2018-06-20 01:21:28 -0300

    Updated roles reducers

--- shared/reducers/lib/form-validation/roles.js
+++ shared/reducers/lib/form-validation/roles.js
@@ -1,8 +1,11 @@
+import _ from 'lodash';
+
 export default function formValidation(state) {
   const isValid = (
     state.fields.name !== '' &&
     !state.fields.nameHasError &&
-    (state.fields.name !== state.originalRol.name)
+    (state.fields.name !== state.originalRol.name ||
+     !_.isEqual(state.fields.permissions, Array.from(state.originalRol.permissions || []).map(p => p.name)))
   );
 
   return {
