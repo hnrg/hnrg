54b03a536c8108506397e0db6051b2c839afecfb
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-06-20 20:19:57 -0300

    Added analytics request and state

---
+++ shared/reducers/states/analytics-state.js
@@ -0,0 +1,27 @@
+export default {
+  demographicDataAnalytics: {
+    totalCount: 0,
+    patientsWithDemographicData: 0,
+    data: {
+      refrigerator: {
+        count: 0,
+        without: 0,
+      },
+      electricity: {
+        count: 0,
+        without: 0,
+      },
+      pet: {
+        count: 0,
+        without: 0,
+      },
+      apartmentType: null,
+      heatingType: null,
+      waterType: null,
+    }
+  },
+  disabled: false,
+  error: null,
+  isValid: false,
+  isFetching: false,
+};
