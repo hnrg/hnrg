771304b7e564516972350dc12151af09a942486f
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-06-20 04:28:07 -0300

    Added demographicData info in patient show

--- shared/components/Patients/Show.jsx
+++ shared/components/Patients/Show.jsx
@@ -1,17 +1,11 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
 import moment from 'moment-timezone';
 import {
-  Button,
   Divider,
   Grid,
   Header,
-  Icon,
   Image,
-  Label,
-  List,
-  Modal,
+  Message,
 } from 'semantic-ui-react';
 
 import icon from 'static/icons/icon.png';
@@ -43,8 +37,47 @@ class Show extends Component {
     });
   }
 
+  showDemographicData() {
+    const { demographicData } = this.state.patient;
+    if (demographicData) {
+      return (
+        <Grid>
+          <Grid.Column style={{paddingBottom: '20px'}} computer={5} largeScreen={5} tablet={16} mobile={16}>
+            <Header as='h4' content='Tiene heladera'/>
+              {demographicData.refrigerator ? "Si" : "No" }
+          </Grid.Column>
+          <Grid.Column style={{paddingBottom: '20px'}} computer={6} largeScreen={6} tablet={16} mobile={16}>
+            <Header as='h4' content='Tiene electricidad'/>
+              {demographicData.electricity ? "Si" : "No" }
+          </Grid.Column>
+          <Grid.Column style={{paddingBottom: '20px'}} computer={5} largeScreen={5} tablet={16} mobile={16}>
+            <Header as='h4' content='Tiene mascotas'/>
+              {demographicData.pet ? "Si" : "No" }
+          </Grid.Column>
+          <Grid.Column style={{paddingBottom: '20px'}} computer={5} largeScreen={5} tablet={16} mobile={16}>
+            <Header as='h4' content='Tipo de vivienda'/>
+              {demographicData.apartmentType.name}
+          </Grid.Column>
+          <Grid.Column style={{paddingBottom: '20px'}} computer={6} largeScreen={6} tablet={16} mobile={16}>
+            <Header as='h4' content='Tipo de calefacción'/>
+              {demographicData.heatingType.name}
+          </Grid.Column>
+          <Grid.Column style={{paddingBottom: '20px'}} computer={5} largeScreen={5} tablet={16} mobile={16}>
+            <Header as='h4' content='Tipo de agua'/>
+              {demographicData.waterType.name}
+          </Grid.Column>
+        </Grid>
+        );
+    } else {
+      return ( <Message
+              info
+              header='El paciente no cuenta con datos demográficos.'
+              content="Puede ir a la sección de 'Editar paciente' para asignarle datos." /> );
+    }
+  }
+
   render() {
-    const { firstName, lastName, address, phone, sex, demographicData, medicalInsurance, documentType, documentNumber } = this.state.patient;
+    const { firstName, lastName, address, phone, sex, medicalInsurance, documentType, documentNumber } = this.state.patient;
 
     const birthday = moment(this.state.patient.birthday);
     birthday.locale('es');
@@ -64,7 +97,7 @@ class Show extends Component {
               <Header as='h4' content='Obra social'/>
               {medicalInsurance && medicalInsurance.name}
             </Grid.Column>
-            <Grid.Column style={{paddingBottom: '20px'}} computer={5} largeScreen={5} tablet={16} mobile={16}>
+            <Grid.Column style={{paddingBottom: '20px'}} computer={6} largeScreen={6} tablet={16} mobile={16}>
               <Header as='h4' content='Tipo de documento'/>
               {documentType && documentType.name}
             </Grid.Column>
@@ -74,11 +107,11 @@ class Show extends Component {
             </Grid.Column>
             <Grid.Column style={{paddingBottom: '20px'}} computer={5} largeScreen={5} tablet={16} mobile={16}>
               <Header as='h4' content='Teléfono'/>
-              {phone}
+              {phone || "---"}
             </Grid.Column>
-            <Grid.Column style={{paddingBottom: '20px'}} computer={5} largeScreen={5} tablet={16} mobile={16}>
+            <Grid.Column style={{paddingBottom: '20px'}} computer={6} largeScreen={6} tablet={16} mobile={16}>
               <Header as='h4' content='Dirección'/>
-              {address}
+              {address || "---"}
             </Grid.Column>
             <Grid.Column style={{paddingBottom: '20px'}} computer={5} largeScreen={5} tablet={16} mobile={16}>
               <Header as='h4' content='Sexo'/>
@@ -86,7 +119,8 @@ class Show extends Component {
             </Grid.Column>
           </Grid.Row>
         </Grid>
-        <Divider />
+        <Divider horizontal>Datos demográficos</Divider>
+        {this.showDemographicData()}
         <Divider hidden />
         <Image size='mini' src={icon} />
       </center>
