8daa6cbf53c5faf4bd798ddc9a8ca97e40d6b37f
lucasdc <ldicunzolo@cespi.unlp.edu.ar>
2018-06-22 00:59:53 -0300

    Added comprobation

--- shared/containers/Analytics/DemographicData/DemographicDataAnayticsPage.jsx
+++ shared/containers/Analytics/DemographicData/DemographicDataAnayticsPage.jsx
@@ -20,12 +20,7 @@ class AnalyticsContainer extends Component {
       this.props.actions.getDemographicDataAnalytics();
     }
   }
-
-  render() {
-    const { demographicDataAnalytics } = this.props.analytics;
-
-    const { totalCount, patientsWithDemographicData } = demographicDataAnalytics;
-
+  showGraphs(data) {
     const {
       refrigerator,
       electricity,
@@ -33,12 +28,7 @@ class AnalyticsContainer extends Component {
       apartmentType,
       heatingType,
       waterType,
-    } = demographicDataAnalytics.data;
-
-    const generalData = {
-      "Pacientes con datos demográficos": patientsWithDemographicData,
-      "Pacientes sin datos demográficos": totalCount - patientsWithDemographicData,
-    };
+    } = data;
 
     const refrigeratorData = {
       "Pacientes con refrigerador": refrigerator.count,
@@ -55,40 +45,57 @@ class AnalyticsContainer extends Component {
     };
 
     return (
+      <Grid textAlign='center'>
+        <Grid.Row>
+          <Grid.Column style={{paddingBottom: '20px'}} computer={8} largeScreen={8} tablet={16} mobile={16}>
+            <Header as='h4' content='Tienen refrigerador' />
+            <PieChart data={refrigeratorData} download='refrigerator-data' />
+          </Grid.Column>
+          <Grid.Column style={{paddingBottom: '20px'}} computer={8} largeScreen={8} tablet={16} mobile={16}>
+            <Header as='h4' content='Tienen electricidad' />
+            <PieChart data={electricityData} download='electricity-data' />
+          </Grid.Column>
+          <Grid.Column style={{paddingBottom: '20px'}} computer={8} largeScreen={8} tablet={16} mobile={16}>
+            <Header as='h4' content='Tienen mascota' />
+            <PieChart data={petData} download='pet-data' />
+          </Grid.Column>
+          <Grid.Column style={{paddingBottom: '20px'}} computer={8} largeScreen={8} tablet={16} mobile={16}>
+            <Header as='h4' content='Tipos de viviendas' />
+            <PieChart data={apartmentType} download='apartmentType-data' />
+          </Grid.Column>
+          <Grid.Column style={{paddingBottom: '20px'}} computer={8} largeScreen={8} tablet={16} mobile={16}>
+            <Header as='h4' content='Tipos de calefacción' />
+            <PieChart data={heatingType} download='heatingType-data' />
+          </Grid.Column>
+          <Grid.Column style={{paddingBottom: '20px'}} computer={8} largeScreen={8} tablet={16} mobile={16}>
+            <Header as='h4' content='Tipos de agua' />
+            <PieChart data={waterType} download='waterType-data' />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
+
+  render() {
+    const { demographicDataAnalytics } = this.props.analytics;
+
+    const { totalCount, patientsWithDemographicData } = demographicDataAnalytics;
+
+    const generalData = {
+      "Pacientes con datos demográficos": patientsWithDemographicData,
+      "Pacientes sin datos demográficos": totalCount - patientsWithDemographicData,
+    };
+
+    return (
       <Segment textAlign='center'>
         <Header as='h2' content='Estadísticas generales sobre datos demográficos' />
         <PieChart data={generalData} download='general-data' />
 
         <Divider horizontal>Datos más específicos</Divider>
-
-        <Grid textAlign='center'>
-          <Grid.Row>
-            <Grid.Column style={{paddingBottom: '20px'}} computer={8} largeScreen={8} tablet={16} mobile={16}>
-              <Header as='h4' content='Tienen refrigerador' />
-              <PieChart data={refrigeratorData} download='refrigerator-data' />
-            </Grid.Column>
-            <Grid.Column style={{paddingBottom: '20px'}} computer={8} largeScreen={8} tablet={16} mobile={16}>
-              <Header as='h4' content='Tienen electricidad' />
-              <PieChart data={electricityData} download='electricity-data' />
-            </Grid.Column>
-            <Grid.Column style={{paddingBottom: '20px'}} computer={8} largeScreen={8} tablet={16} mobile={16}>
-              <Header as='h4' content='Tienen mascota' />
-              <PieChart data={petData} download='pet-data' />
-            </Grid.Column>
-            <Grid.Column style={{paddingBottom: '20px'}} computer={8} largeScreen={8} tablet={16} mobile={16}>
-              <Header as='h4' content='Tipos de viviendas' />
-              <PieChart data={apartmentType} download='apartamentType-data' />
-            </Grid.Column>
-            <Grid.Column style={{paddingBottom: '20px'}} computer={8} largeScreen={8} tablet={16} mobile={16}>
-              <Header as='h4' content='Tipos de calefacción' />
-              <PieChart data={heatingType} download='heatingType-data' />
-            </Grid.Column>
-            <Grid.Column style={{paddingBottom: '20px'}} computer={8} largeScreen={8} tablet={16} mobile={16}>
-              <Header as='h4' content='Tipos de agua' />
-              <PieChart data={waterType} download='waterType-data' />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+        { patientsWithDemographicData > 0 ? 
+          this.showGraphs(demographicDataAnalytics.data) :
+          <Header as='h4' content='No hay datos para mostrar' />
+        }
       </Segment>
     );
   }
