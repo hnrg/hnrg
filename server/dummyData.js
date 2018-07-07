d30662270e94d3c12f1d15ddb886a2fcda889bc8
ulises-jeremias <ulisescf.24@gmail.com>
2018-07-07 12:05:07 -0300

    Updated dummy data

--- server/dummyData.js
+++ server/dummyData.js
@@ -5,7 +5,11 @@ const Configuration = require('./models/configuration');
 const Permission = require('./models/permission');
 const Rol = require('./models/rol');
 const User = require('./models/user');
-
+const ApartmentType = require('./models/apartment-type');
+const DocumentType = require('./models/document-type');
+const HeatingType = require('./models/heating-type');
+const MedicalInsurance = require('./models/medical-insurance');
+const WaterType = require('./models/water-type');
 
 const dummyData = async function dummyData() {
   await Permission.count().exec((err, count) => {
@@ -185,6 +189,150 @@ const dummyData = async function dummyData() {
           });
         });
       });
+    });
+  });
+
+  await ApartmentType.count().exec((err, count) => {
+    if (err) {
+      throw err;
+    }
+
+    console.log(count);
+    if (count > 0) {
+      return;
+    }
+
+    const apartmentTypes = [
+      'Casa tipo A',
+      'Casa tipo B',
+      'Rancho',
+      'Casilla',
+      'Departamento',
+      'Pieza/s en iniquilato',
+      'Pieza/s en hotel o pension',
+      'Local no construido para habitacion',
+      'Vivienda movil'
+    ];
+
+    ApartmentType.create(apartmentTypes.map(name => new ApartmentType({ name })), ($err, saved) => {
+      if ($err) {
+        throw $err;
+      }
+    });
+  });
+
+  await DocumentType.count().exec((err, count) => {
+    if (err) {
+      throw err;
+    }
+
+    if (count > 0) {
+      return;
+    }
+
+    const documentTypes = [
+      'DNI',
+      'LC',
+      'LE',
+      'CI',
+      'PAS'
+    ];
+
+    DocumentType.create(documentTypes.map(name => new DocumentType({ name })), ($err, saved) => {
+      if ($err) {
+        throw $err;
+      }
+    });
+  });
+
+  await HeatingType.count().exec((err, count) => {
+    if (err) {
+      throw err;
+    }
+
+    if (count > 0) {
+      return;
+    }
+
+    const heatingTypes = [
+      'Estufa gas',
+      'Estufa electrica',
+      'Caldera, radiadores o losa radiante',
+      'Aire acondicionado frio/calor',
+      'Bomba de calor',
+      'Hogar a leña',
+      'Hogar a gas'
+    ];
+
+    HeatingType.create(heatingTypes.map(name => new HeatingType({ name })), ($err, saved) => {
+      if ($err) {
+        throw $err;
+      }
+    });
+  });
+
+  await WaterType.count().exec((err, count) => {
+    if (err) {
+      throw err;
+    }
+
+    if (count > 0) {
+      return;
+    }
+
+    const waterTypes = [
+      'Agua corriente',
+      'Perforación',
+      'Pozo',
+      'Agua de lluvia',
+      'Transporte por cisterna',
+      'Río, canal, arroyo'
+    ];
+
+    WaterType.create(waterTypes.map(name => new WaterType({ name })), ($err, saved) => {
+      if ($err) {
+        throw $err;
+      }
+    });
+  });
+
+  await MedicalInsurance.count().exec((err, count) => {
+    if (err) {
+      throw err;
+    }
+
+    if (count > 0) {
+      return;
+    }
+
+    const medicalInsurances = [
+      'OSDE',
+      'Sancor Salud',
+      'Medicus',
+      'Medifé',
+      'Galeno',
+      'Accord Salud',
+      'OMINT',
+      'Swiss Medical',
+      'AcaSalud',
+      'Bristol Medicine',
+      'OSECAC',
+      'Union Personal',
+      'OSPACP',
+      'OSDEPYM',
+      'Luis Pasteur',
+      'OSMEDICA',
+      'IOMA',
+      'OSPJN',
+      'OSSSB',
+      'OSPEPBA',
+      'OSPE'
+    ];
+
+    MedicalInsurance.create(medicalInsurances.map(name => new MedicalInsurance({ name })), ($err, saved) => {
+      if ($err) {
+        throw $err;
+      }
     });
   });
 };
--- tests/server/auth.test.js
+++ tests/server/auth.test.js
@@ -6,21 +6,20 @@ const Seed = require('../../server/dummyData');
 const Rol = require('../../server/models/rol');
 const User = require('../../server/models/user');
 
-(function cleanAndSeedDb() {
-  User.remove({});
-  Rol.remove({});
-  Seed();
-}());
-
 describe('Login Tests', () => {
   test('should return the token if valid', (done) => {
     request(server)
       .post('/api/auth/login')
       .send({
-        email: 'admin@hnrg.com',
-        password: 'hnrg-admin',
+        email: 'su@hnrg.com',
+        password: 'hnrg-su',
       })
       .end((err, res) => {
+        if (err) {
+          throw err;
+        }
+
+        console.log(res);
         const { token } = res.body;
         expect(token).to.be.an('string');
         expect(res.statusCode).to.be.equal(200);
